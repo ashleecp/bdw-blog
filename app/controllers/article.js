3ed4aab89ec8c4094ac2f36134b3fa53db382511
Ashlee Perry <ashleeperry@gmail.com>
2015-04-14 19:13:21 -0600

    added a show route

--- app/controllers/article.js
+++ app/controllers/article.js
@@ -13,14 +13,25 @@ router.get('/', function (req, res, next) {
     Article.find({}, function(err, articles){
       console.log('articles: ', articles);
 
-      res.send('article/list', {
+      res.render('article/list', {
       title: 'My Articles',
       articles: articles
       });
-    });
-  
+    });        
+});
+
+router.get('/:id', function (req, res, next) {
+
+    var id = req.params.id;
     
-  
+    Article.findOne({ _id:id }, function(err, articles){
+      
+
+      res.render('article/show', {
+      title: 'My Articles',
+      articles: articles
+      });
+    });        
 });
 
 // router.get('/bootstrap', function(req, res, next) {
